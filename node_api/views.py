8beaaf5a9fc7f31382cef9f0047fff8cfcf586c3
Rahul Jadhav <jrahul1112@gmail.com>
2022-06-13 23:12:10 +0530

    Updated Node APIs

--- node_api/views.py
+++ node_api/views.py
@@ -1,51 +1,88 @@
-import json
-import requests
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from tpc_api.models import Student
+import requests
+import json
 
 
 
 class GetAllStudents(APIView):
 
     def get(self, request):
-        url = "https://tpc-backend-node.herokuapp.com/filter"
-        payload = ""
-        headers = {}
-        response = requests.request("GET", url, headers=headers, data=payload)
-        return Response(response.json())
+        jwt_roll_no = request.user.roll_no.strip()
+        userRecord = Student.objects.get(roll_no=jwt_roll_no)
+
+        if userRecord.groups.filter(name='Superuser'):
+            url = "https://tpc-backend-node.herokuapp.com/filter"
+            payload = ""
+            headers = {}
+            response = requests.request("GET", url, headers=headers, data=payload)
+            return Response(response.json())
+        else:
+            return Response(
+                {'status': 'Error', 'message': 'Insufficient Permissions to perform the request'}, 
+                status = 403
+            )
 
 
 class GetStudentByRollNo(APIView):
 
     def post(self, request):
         roll_no = request.data['roll_no']
-        url = "https://tpc-backend-node.herokuapp.com/filter/" + roll_no
-        payload = {}
-        headers = {}
-        response = requests.request("GET", url, headers=headers, data=payload)
-        return Response(response.json())
+        jwt_roll_no = request.user.roll_no.strip()
+        userRecord = Student.objects.get(roll_no=jwt_roll_no)
+
+        if userRecord.groups.filter(name='Superuser'):
+            url = "https://tpc-backend-node.herokuapp.com/filter/" + roll_no
+            payload = {}
+            headers = {}
+            response = requests.request("GET", url, headers=headers, data=payload)
+            return Response(response.json())
+        else:
+            return Response(
+                {'status': 'Error', 'message': 'Insufficient Permissions to perform the request'}, 
+                status = 403
+            )
 
 
 class GetStudentsByDept(APIView):
 
     def post(self, request):
         department = request.data['department']
-        url = "https://tpc-backend-node.herokuapp.com/filter/dept/" + department
-        payload = {}
-        headers = {}
-        response = requests.request("GET", url, headers=headers, data=payload)
-        return Response(response.json())
+        jwt_roll_no = request.user.roll_no.strip()
+        userRecord = Student.objects.get(roll_no=jwt_roll_no)
+
+        if userRecord.groups.filter(name='Superuser'):
+            url = "https://tpc-backend-node.herokuapp.com/filter/dept/" + department
+            payload = {}
+            headers = {}
+            response = requests.request("GET", url, headers=headers, data=payload)
+            return Response(response.json())
+        else:
+            return Response(
+                {'status': 'Error', 'message': 'Insufficient Permissions to perform the request'}, 
+                status = 403
+            )
 
 
 class GetStudentProfile(APIView):
 
     def post(self, request):
         roll_no = request.data['roll_no']
-        url = "https://tpc-backend-node.herokuapp.com/filter/profile/" + roll_no
-        payload = ""
-        headers = {}
-        response = requests.request("GET", url, headers=headers, data=payload)
-        return Response(response.json())
+        jwt_roll_no = request.user.roll_no.strip()
+        userRecord = Student.objects.get(roll_no=jwt_roll_no)
+
+        if userRecord.groups.filter(name='Superuser'):
+            url = "https://tpc-backend-node.herokuapp.com/filter/profile/" + roll_no
+            payload = ""
+            headers = {}
+            response = requests.request("GET", url, headers=headers, data=payload)
+            return Response(response.json())
+        else:
+            return Response(
+                {'status': 'Error', 'message': 'Insufficient Permissions to perform the request'}, 
+                status = 403
+            )
 
 
 class Dashboard(APIView):
@@ -55,24 +92,32 @@ class Dashboard(APIView):
         sem_pointer = request.data['sem_pointer'].lower()
         project = request.data['project'].lower()
         hobbies = request.data['hobbies'].lower()
-        
-        fields = {'rollno': True}
-        if hobbies == 'true':
-            fields['hobbies'] = hobbies.capitalize()
-        if sem_pointer == 'true':
-            fields['sem_pointer'] = sem_pointer.capitalize()
-        if project == 'true':
-            fields['project'] = project.capitalize()
+        jwt_roll_no = request.user.roll_no.strip()
+        userRecord = Student.objects.get(roll_no=jwt_roll_no)
 
-        url = "https://tpc-backend-node.herokuapp.com/filter/dashboard"
-        payload = json.dumps({
-            "fields": fields,
-            "queries": {
-                "rollno": roll_no
+        if userRecord.groups.filter(name='Superuser'):
+            fields = {'rollno': True}
+            if hobbies == 'true':
+                fields['hobbies'] = hobbies.capitalize()
+            if sem_pointer == 'true':
+                fields['sem_pointer'] = sem_pointer.capitalize()
+            if project == 'true':
+                fields['project'] = project.capitalize()
+
+            url = "https://tpc-backend-node.herokuapp.com/filter/dashboard"
+            payload = json.dumps({
+                "fields": fields,
+                "queries": {
+                    "rollno": roll_no
+                }
+            })
+            headers = {
+                'Content-Type': 'application/json'
             }
-        })
-        headers = {
-            'Content-Type': 'application/json'
-        }
-        response = requests.request("POST", url, headers=headers, data=payload)
-        return Response(response.json())
+            response = requests.request("POST", url, headers=headers, data=payload)
+            return Response(response.json())
+        else:
+            return Response(
+                {'status': 'Error', 'message': 'Insufficient Permissions to perform the request'}, 
+                status = 403
+            )
